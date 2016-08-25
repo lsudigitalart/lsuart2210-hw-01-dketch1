72ef1905ef5adf02b258e0d13819f35389a6e61e
Dominique M Ketchens <dketch1@DESN-art328-09.lsu.edu>
2016-08-25 11:47:22 -0500

    not done

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+createCanvas(500, 500, P2D);
+ellipseMode(CENTER);
+fill(50);
+ellipse(250, 0, 300, 300);
+ellipse(250, 0, 200, 200);
+ellipse(250, 0, 100, 100);
+ellipse(250, 0, 50, 50);
+ellipse(250, 500, 300, 300);
+ellipse(250, 500, 200, 200);
+ellipse(250, 500, 100, 100);
+ellipse(250, 500, 50, 50);
+ellipse(200, 150, 110, 110);
+ellipse(60, 340, 50, 50);
+ellipse(160, 340, 50, 50);
+ellipse(340, 340, 50, 50);
+ellipse(440, 340, 50, 50);
+ellipse(480, 10, 20, 20);
+ellipse(450, 50, 40, 40);
+ellipse(420, 90, 60, 60);
+ellipse(390, 130, 80, 80);
+ellipse(360, 170, 100, 100);
+}
